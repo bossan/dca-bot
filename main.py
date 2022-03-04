5597732dd9e11c70c3871941b1244cf81963cf5c
Sander Bos <sanderbos17@icloud.com>
2022-03-04 19:51:17 +0100

    Updated script

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging, time, sys
+
 from decouple import config
 from python_bitvavo_api.bitvavo import Bitvavo
 
@@ -5,74 +7,63 @@ from python_bitvavo_api.bitvavo import Bitvavo
 COINS = ['BTC']
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
 def buy(client: Bitvavo, amount, coin):
+    logger.info("Buying BTC")
     return client.placeOrder('%s-EUR' % coin, 'buy', 'market', {'amountQuote': str(amount)})
 
 
 def send_to_ledger(client: Bitvavo, amount):
     address = config('LEDGER_ADDRESS', default='')
+    logger.info("Sending ₿ %f to %s", amount, address)
     return client.withdrawAssets('BTC', amount, address, {})
 
 
-def main():
-    client = Bitvavo({
-        'APIKEY': config('API_KEY', default=''),
-        'APISECRET': config('API_SECRET', default=''),
-        'RESTURL': 'https://api.bitvavo.com/v2',
-        'WSURL': 'wss://ws.bitvavo.com/v2/',
-        'ACCESSWINDOW': 10000,
-        'DEBUGGING': False
-    })
-
-    total_crypto = {}
-
-    total_paid = 0.0
-    total_received = 0.0
-    total_fees_paid = 0.0
+def run(client: Bitvavo):
+    logger.info("Started new run")
 
     total_balance_eur_in_order = 0.0
     total_balance_eur_available = 0.0
-
     total_balance_btc_available = 0.0
     total_balance_btc_in_order = 0.0
-
-    total_deposit = 0.0
-    total_deposit_fee = 0.0
-
-    total_value = 0.0
-
-    print("""
-╔═══╗╔═══╗╔═══╗    ╔══╗ ╔═══╗╔════╗
-╚╗╔╗║║╔═╗║║╔═╗║    ║╔╗║ ║╔═╗║║╔╗╔╗║
- ║║║║║║ ╚╝║║ ║║    ║╚╝╚╗║║ ║║╚╝║║╚╝
- ║║║║║║ ╔╗║╚═╝║    ║╔═╗║║║ ║║  ║║  
-╔╝╚╝║║╚═╝║║╔═╗║    ║╚═╝║║╚═╝║ ╔╝╚╗ 
-╚═══╝╚═══╝╚╝ ╚╝    ╚═══╝╚═══╝ ╚══╝ """)
-    print("-----------------------------------")
 
     # Check if we have EUR balance
     for balance in client.balance({'symbol': 'EUR'}):
         total_balance_eur_available += float(balance.get('available'))
         total_balance_eur_in_order += float(balance.get('inOrder'))
 
-    print("Available € %f" % total_balance_eur_available)
+    logger.info("Available € %f", total_balance_eur_available)
     # If we have EUR balance, buy BTC
     if total_balance_eur_available > 1.0:
-        print("Buying BTC")
         order = buy(client, total_balance_eur_available, 'BTC')
-        print("Order: %s" % order)
+        logger.debug("Order: %s", order)
 
     # Check if we have BTC balance
     for balance in client.balance({'symbol': 'BTC'}):
         total_balance_btc_available += float(balance.get('available'))
         total_balance_btc_in_order += float(balance.get('inOrder'))
 
-    print("Available ₿ %f" % total_balance_btc_available)
+    logger.info("Available ₿ %f" % total_balance_btc_available)
     # If we have BTC balance, send it to Ledger Wallet
     if total_balance_btc_available > 0:
-        print("Sending to ledger")
         withdrawal = send_to_ledger(client, total_balance_btc_available)
-        print("Withdrawal: %s" % withdrawal)
+        logger.debug("Withdrawal: %s", withdrawal)
+
+    logger.info("Finished run")
+
+
+def calculate_result(client: Bitvavo):
+    total_crypto = {}
+
+    total_paid = 0.0
+    total_received = 0.0
+    total_fees_paid = 0.0
+    total_deposit = 0.0
+    total_deposit_fee = 0.0
+    total_value = 0.0
 
     for deposit in client.depositHistory({'symbol': 'EUR'}):
         total_deposit += float(deposit.get('amount'))
@@ -92,12 +83,39 @@ def main():
 
         total_value += price * total_crypto.get(coin, 0.0)
 
-    print("-----------------------------------")
-    print("TOTAL PAID: € %f" % total_deposit)
-    print("TOTAL VALUE: € %f" % total_value)
-    print("RESULT: %f%%" % (((total_value - total_deposit) / total_deposit) * 100))
-    print("-----------------------------------")
+    logger.info("TOTAL PAID: € %f" % total_deposit)
+    logger.info("TOTAL VALUE: € %f" % total_value)
+    logger.info("RESULT: %f%%" % (((total_value - total_deposit) / total_deposit) * 100))
+
+
+def main():
+    while True:
+        client = Bitvavo({
+            'APIKEY': config('API_KEY', default=''),
+            'APISECRET': config('API_SECRET', default=''),
+            'RESTURL': 'https://api.bitvavo.com/v2',
+            'WSURL': 'wss://ws.bitvavo.com/v2/',
+            'ACCESSWINDOW': 10000,
+            'DEBUGGING': False
+        })
+
+        run(client)
+        time.sleep(60 * 60)
 
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(format='%(asctime)s %(message)s')
+
+    logger.info("""Starting
+    ╔═══╗╔═══╗╔═══╗    ╔══╗ ╔═══╗╔════╗
+    ╚╗╔╗║║╔═╗║║╔═╗║    ║╔╗║ ║╔═╗║║╔╗╔╗║
+     ║║║║║║ ╚╝║║ ║║    ║╚╝╚╗║║ ║║╚╝║║╚╝
+     ║║║║║║ ╔╗║╚═╝║    ║╔═╗║║║ ║║  ║║  
+    ╔╝╚╝║║╚═╝║║╔═╗║    ║╚═╝║║╚═╝║ ╔╝╚╗ 
+    ╚═══╝╚═══╝╚╝ ╚╝    ╚═══╝╚═══╝ ╚══╝ """)
+
+    try:
+        logger.info("Press CTRL + C to stop the process")
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
